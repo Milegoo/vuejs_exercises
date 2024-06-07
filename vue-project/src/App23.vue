13cd0a8d12b10c8bddda3719efb1e2335a205fa4
Eloi Milego <eloimilego@gmail.com>
2024-06-08 01:31:30 +0200

    all

---
+++ vue-project/src/App23.vue
@@ -0,0 +1,32 @@
+<script>
+import MagicInput from './components/MagicInput.vue'
+export default {
+    components: {
+        MagicInput
+    },
+
+    data() {
+        return {
+            buffer: '',
+            reversedBuffer: '',
+        }
+    },
+
+    watch: {
+
+        buffer: function () {
+            this.reversedBuffer = this.buffer.replace(/./g, x => x.toUpperCase() == x ? x.toLowerCase() : x.toUpperCase())
+        }
+
+    }
+
+
+
+}
+</script>
+<template>
+    <MagicInput v-model:buffer="buffer" v-model:reversedBuffer="reversedBuffer"/>
+    <input type="text" v-model="buffer" /><span>{{ buffer }}</span>
+
+</template>
+<style></style>
---
+++ vue-project/src/components/MagicInput.vue
@@ -0,0 +1,38 @@
+<script>
+export default{
+
+    props: ['buffer', 'reversedBuffer'],
+    emits: ['update:buffer', 'update:reversedBuffer'],
+
+    data() {
+        return {
+            changedText: '',
+        }
+    },
+
+    watch: {
+
+        buffer: function() {
+            this.changedText = this.buffer.replace(/./g,x => x.toUpperCase() == x ? x.toLowerCase() : x.toUpperCase())
+        },
+
+        reversedBuffer: function() {
+            this.changedText = this.reversedBuffer
+        },
+
+        changedText: function(newValue) {
+            this.$emit('update:reversedBuffer', newValue)
+            let newBuffer = this.changedText.replace(/./g,x => x.toUpperCase() == x ? x.toLowerCase() : x.toUpperCase())
+            this.$emit('update:buffer', newBuffer)
+        }
+
+    }
+
+
+}
+</script>
+<template>
+    <input type="text" v-model="changedText"/>
+</template>
+<style>
+</style>
